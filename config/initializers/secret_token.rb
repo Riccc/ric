9874b872feb28472bf9485a9e4016f1208b807e3
唐民鹏 <minpeng.tang@hand-china.com>
2016-07-18 17:32:35 +0800

    first_try

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ric::Application.config.secret_token = 'ab017ba23d2cdc8297979fe80253adac24d8b1c965a5b09926cffc12fdab4a0899bed9828feaf468f361a6790af9e9b04e6afa08a9b83fa73df7299ff9a8d586'
